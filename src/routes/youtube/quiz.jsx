cce2f3fa438f80fb93049b1eebb217f88f946fd2
Zahra Mirali <z.mirali1997@gmail.com>
2024-07-19 13:53:55 +0200

    use isChecked

--- src/routes/youtube/quiz.jsx
+++ src/routes/youtube/quiz.jsx
@@ -7,6 +7,7 @@ export default function YoutubeQuiz() {
   const [constructedSentence, setConstructedSentence] = useState([]);
   const [feedback, setFeedback] = useState("");
   const [inputIndex, setInputIndex] = useState(0);
+  const [isChecked, setIsChecked] = useState(true);
 
   useEffect(() => {
     async function initializeState() {
@@ -50,14 +51,24 @@ export default function YoutubeQuiz() {
     setFeedback("");
   };
 
+  const handleClearSentence = () => {
+    setConstructedSentence([]);
+  };
+
   return (
     <div className="p-4 flex flex-col justify-between min-h-screen">
       <div className="flex flex-col gap-4">
         <div className="font-semibold">
           #{questionIndex}: {objectsArray[questionIndex]?.meaning}
         </div>
-        <div className="p-4 border rounded h-56">
+        <div className="relative p-4 border rounded h-56">
           {constructedSentence.join(" ")}
+          <button
+            onClick={handleClearSentence}
+            className="absolute top-2 right-2 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            Clear
+          </button>
         </div>
         {feedback && (
           <div className="font-semibold">
@@ -78,27 +89,48 @@ export default function YoutubeQuiz() {
           ))}
         </div>
 
-        <div className="font-semibold h-6">{feedback}</div>
-        <div className="flex justify-between">
-          <button
-            onClick={handleCheckSentence}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Check
-          </button>
+        <div
+          className={`font-semibold h-6 self-start px-3 rounded text-white ${
+            feedback === "Correct!"
+              ? "bg-green-500"
+              : feedback
+              ? "bg-red-500"
+              : ""
+          }`}
+        >
+          {feedback}
+        </div>
+        <div className="flex justify-between items-center">
+          <div>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={() => setIsChecked(!isChecked)}
+            />
+            <label className="ml-2">Check?</label>
+          </div>
           <div className="flex gap-2">
             <input
               type="number"
-              className="px-2 py-1 border rounded w-12"
+              className="px-2 py-1 border rounded w-32"
               value={inputIndex}
               onChange={(e) => setInputIndex(e.target.value)}
             />
-            <button
-              onClick={handleNextQuestion}
-              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-            >
-              Go
-            </button>
+            {isChecked && !feedback ? (
+              <button
+                onClick={handleCheckSentence}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-32"
+              >
+                Check
+              </button>
+            ) : (
+              <button
+                onClick={handleNextQuestion}
+                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 w-32"
+              >
+                Go
+              </button>
+            )}
           </div>
         </div>
       </div>
